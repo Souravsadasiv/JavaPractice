b1d9a5ec8ac0e762ec345056c15dc3f1ec890db4
Sourav <sourabhasikun20@gmail.com>
2023-05-03 18:56:35 +0530

    minmaxelement

---
+++ src/com/java/DSA/Array/MinMaxEl.java
@@ -0,0 +1,19 @@
+package com.java.DSA.Array;
+
+public class MinMaxEl {
+    public static void main(String[] args) {
+        int[] arr={10,50,30,2,9,23,49,60};
+        int min=arr[0];
+        int max=arr[0];
+        for (int i = 1; i < arr.length; i++) {
+            if(arr[i]<min){
+                min=arr[i];
+            }if(arr[i]>max){
+                max=arr[i];
+            }
+        }
+        System.out.println("Max ="+max);
+        System.out.println("Min ="+min);
+    }
+
+}
